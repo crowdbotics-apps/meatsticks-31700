eb732d409740c343b724526301a3b8e050d565b3
Crowdbotics <team@crowdbotics.com>
2021-10-26 02:57:07 +0000

    Update models

--- backend/meatsticks_31700/wsgi.py
+++ backend/meatsticks_31700/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'meatsticks_31700.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "meatsticks_31700.settings")
 
 application = get_wsgi_application()
